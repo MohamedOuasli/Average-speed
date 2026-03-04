a94cca5fbf07f03fd8efa7717eb2005435a329e5
Pancakeloki <dodoouasli@gmail.com>
2026-03-03 19:44:09 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+a=int(input("Enter the speed: "))
+b=int(input("Enter the speed: "))
+c=int(input("Enter the speed: "))
+d=a+b+c
+e=d/3
+
+if (a<e):
+    print("a is the slowest")
+
+elif (b<e):
+    print("b is the slowest")
+
+else:
+    print("c is the slowest")
